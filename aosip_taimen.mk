cd4e34175ac9ac578b7055a59f873f6c3daa3fa8
Eamon Powell <eamonpowell@outlook.com>
2020-04-08 08:31:20 +1000

    AOSiP-ify
    Change-Id: I94c44f983c4aeb48bcac9afbf5c9ea4f939acc78

--- aosip_taimen.mk
+++ aosip_taimen.mk
@@ -2,16 +2,16 @@
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
--include device/google/taimen/device-lineage.mk
+-include device/google/wahoo/device-aosip.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_taimen
+PRODUCT_NAME := aosip_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 PRODUCT_RESTRICT_VENDOR_FILES := false
