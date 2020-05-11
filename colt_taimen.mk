ca40412b6183d106a703cf485ae57d54801dbdc8
nitin1438 <nitin.chobhe@gmail.com>
2020-05-11 22:08:30 +0530

    Clean up and some changes

--- colt_taimen.mk
+++ colt_taimen.mk
@@ -2,7 +2,7 @@
 TARGET_BOOT_ANIMATION_RES := 1440x2880
 
 # Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/colt/config/common.mk)
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
@@ -15,15 +15,14 @@ PRODUCT_DEVICE := taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 PRODUCT_MANUFACTURER := Google
-COLT_BUILD_TYPE := OFFICIAL
+COLT_BUILD_TYPE := Official
 PRODUCT_RESTRICT_VENDOR_FILES := false
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.colt.maintainer="nitin.chobhe"
+COLT_DEVICE_MAINTAINER := nitin.chobhe
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 10 QQ2A.200501.001.B3 6396602 release-keys"
+	PRODUCT_NAME="taimen" \
+	TARGET_DEVICE="taimen" \
+	PRIVATE_BUILD_DESC="taimen-user 10 QQ2A.200501.001.B3 6396602 release-keys"
 
 BUILD_FINGERPRINT := google/taimen/taimen:10/QQ2A.200501.001.B3/6396602:user/release-keys
 
